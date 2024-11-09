af17efaf3730619771386f6b8d27ebcfa0eb4e38
joshue04 <joshjaq94@gmail.com>
2024-11-09 15:42:11 -0500

    Data cleanup

--- Data/Bathroom_data.py
+++ Data/Bathroom_data.py
@@ -28,15 +28,23 @@ def fetch_all_data_csv(base_url, limit=500, total_rows=10000):
 data = fetch_all_data_csv("https://data.cityofnewyork.us/resource/i7jb-7jku.csv")
 
 data['changing_stations'] = data['changing_stations'].fillna('No')
-data['changing_stations'] = data['changing_stations'].map({'No':0,'Yes':1})
+data['changing_stations'] = data['changing_stations'].replace('^\s*$',' ',regex = True)
+data['changing_stations'] = data['changing_stations'].map({'':0,' ':0,'No':0,'Yes':1})
+data['status'] = data['status'].replace('^\s*$','Not Operational',regex = True)
 data['status'] = data['status'].fillna('Not Operational')
-data['status'] = data['status'].map({'Not Operational':0,'Operational':1})
+data['status'] = data['status'].map({'':0,' ':0,'Not Operational':0,'Operational':1})
+data['open'] = data['open'].replace('^\s*$','',regex = True)
 Seasonal_index = data[data['open']=='Seasonal'].index
 Future_index =  data[data['open']=='Future'].index
+Empty_index =  data[data['open']==''].index
+Empty2_index = data[data['open']==' '].index
 data.drop(Seasonal_index,inplace=True)
 data.drop(Future_index,inplace=True)
+data.drop(Empty_index,inplace=True)
+data.drop(Empty2_index,inplace=True)
+
 
 data.to_csv("Data/Parsed_CSV.csv",index=False)
-print(data.value_counts())
+# print(data.value_counts())
 
 print(data.head)
